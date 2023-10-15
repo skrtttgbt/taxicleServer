3e6f63b6b609d3d917b5522706cfd9db87534d22
skrtttgbt <geraldbuscayno@gmail.com>
2023-10-16 00:30:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from "cors"
 
 const app = express ()
 app.use(cors({
-    origin: ['https://taxicle-app.vercel.app', 'http://localhost:3000'] , // Specify the allowed origin (your frontend app)
+    origin: ['https://taxicle-app.vercel.app'] , // Specify the allowed origin (your frontend app)
     methods: ["POST", "GET"],
     credentials: true, 
 }))
